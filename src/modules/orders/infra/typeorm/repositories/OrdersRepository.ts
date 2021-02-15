4195f2ea315f88c9d41261887ba45cc8dd6f5afc
Lucas Maniero <lucasfomaniero@gmail.com>
2021-02-15 18:27:53 -0300

    challenge completed

--- src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
+++ src/modules/orders/infra/typeorm/repositories/OrdersRepository.ts
@@ -12,11 +12,21 @@ class OrdersRepository implements IOrdersRepository {
   }
 
   public async create({ customer, products }: ICreateOrderDTO): Promise<Order> {
-    // TODO
+    const order = await this.ormRepository.save({
+      customer,
+      order_products: products,
+    });
+
+    await this.ormRepository.save(order);
+    return order;
   }
 
   public async findById(id: string): Promise<Order | undefined> {
-    // TODO
+    // TODO: COMO BUSCAR AS RELACOES TBM
+    const foundOrder = await this.ormRepository.findOne(id, {
+      relations: ['order_products', 'customer'],
+    });
+    return foundOrder;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1613397775952-Products.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class Products1613397775952 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'price',
+            type: 'decimal',
+            isNullable: false,
+            precision: 10,
+            scale: 2,
+          },
+          {
+            name: 'quantity',
+            type: 'integer',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropTable('products');
+  }
+}
